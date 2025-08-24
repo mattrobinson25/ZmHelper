c5706f18696122de404e6d73f29d52209fdbd5eb
root <mattrobinson25@protonmail.com>
2025-08-24 11:11:14 -0400

    format improvements

--- zm_move.py
+++ zm_move.py
@@ -102,7 +102,7 @@ delete_size: int = 0
 
 if allow_delete:
     for cache in listdir(save_dir):
-        cache_dir = f'{save_dir}/{cache}'
+        cache_dir: str = f'{save_dir}/{cache}'
 
         for date_dir in listdir(cache_dir):
             try:
@@ -138,7 +138,7 @@ if allow_delete:
         ''')
 
     [task.start() for task in zm_helper.delete_threads]
-    [task.join() for task in zm_helper.delete_threads]
+    [task.join()  for task in zm_helper.delete_threads]
 else:
     logger.info('Deletion disabled')
 
@@ -162,31 +162,31 @@ if allow_move:
         for date_dir in dates_cache:
             if limit_reached:
                 continue
-            else:
-                try:
-                    cache_date_parsed: dt = dt.strptime(date_dir, date_fmt)  # Convert dir name into datetime object
-                except ValueError:
-                    logger.warning(f'Invalid date folder found {zm_dir}/{cache}/{date_dir}. This should be deleted!')
-                    continue
 
-                if cache_date_parsed < time_threshold:
-                    source: str = f'{zm_dir}/{cache}/{date_dir}'
-                    destination: str = f'{save_dir}/{cache}/{date_dir}'
+            try:
+                cache_date_parsed: dt = dt.strptime(date_dir, date_fmt)  # Convert dir name into datetime object
+            except ValueError:
+                logger.warning(f'Invalid date folder found {zm_dir}/{cache}/{date_dir}. This should be deleted!')
+                continue
 
-                    if len(zm_helper.archive_threads) < max_threads:  # Put a limit on how many jobs can be done per day
-                        logger.debug(f'Found copy path {source}')
-                        size: int = df[df.date == date_dir][cache].item()
-                        backup_size += size
+            if cache_date_parsed < time_threshold:
+                source: str = f'{zm_dir}/{cache}/{date_dir}'
+                destination: str = f'{save_dir}/{cache}/{date_dir}'
 
-                        # archive_worker and move_worker will have the same args
-                        thread: Thread = Thread(
-                            target=zm_helper.archive_worker,
-                            args=(source, destination, size, cache, date_dir, 'bztar')
-                        )
-                        zm_helper.archive_threads.append(thread)
-                    else:
-                        logger.warning(f'Maximum of {max_threads} move jobs reached for the day.')
-                        limit_reached: bool = True
+                if len(zm_helper.archive_threads) < max_threads:  # Put a limit on how many jobs can be done per day
+                    logger.debug(f'Found copy path {source}')
+                    size: int = df[df.date == date_dir][cache].item()
+                    backup_size += size
+
+                    # archive_worker and move_worker will have the same args
+                    thread: Thread = Thread(
+                        target=zm_helper.archive_worker,
+                        args=(source, destination, size, cache, date_dir, 'bztar')
+                    )
+                    zm_helper.archive_threads.append(thread)
+                else:
+                    logger.warning(f'Maximum of {max_threads} move jobs reached for the day.')
+                    limit_reached: bool = True
 
 
 disk_availability_end: int = backup_vol.disk_available()
@@ -255,40 +255,41 @@ logger.debug(f'{disk_used_end} - {disk_used_start} = {disk_used_end - disk_used_
 # disk_change will be interpolated into a str. A plus or minus sign will be put in front of it as a str
 if disk_change > 0:
     sign: str = '+'
-elif disk_change == 0:
-    sign: str = ''
-else:
+elif disk_change < 0:
     sign: str = '-'
+else:  # equal to 0
+    sign: str = ''
 
 disk_change: int = abs(disk_change)
 
-# very often disk_usage_pcent will have a val of 0% when very small changes have been made. This will be re-defined as <1%
-if disk_usage_pcent == 0:
-    disk_usage_pcent: str = f'< {sign}1'
-if disk_change == 0:
-    disk_usage_pcent: int = 0
+if disk_usage_pcent == 0 and disk_change != 0:
+    # Disk_usage_pcent will round to 0 when very small changes are made. This will be re-defined as <1%
+    disk_usage_pcent: str = '<1'
 
+if disk_change == 0:
+    # This will only pass when nothing has changed. disk_usage_pcent will also equal 0
+    disk_usage_pcent: str = '0'
 
 logger.warning(f'''
             Status: {status.upper()}
           Run time: {dt.now() - start}  
         Backup Job: {backup_size_human_readable}
         Delete Job: {delete_size_human_readable}
-    Change on Disk: {sign}{byte_sizer(disk_change)} ({sign}{disk_usage_pcent}%)
+    Change on Disk: {sign}{byte_sizer(disk_change)} ({disk_usage_pcent}%)
     
     {byte_sizer(backup_vol.disk_available())} remaining on backup disk.
     Backup disk usage is at {disk_usage_end}%.
 ''')
 
 if disk_usage_end >= 90:
-    message: str = f'Warning! ZM backup cache is at {disk_usage_end}%'
-    date_and_time: str = dt.now().strftime('%c')
-    full_message: str = f"{date_and_time} -- {message}\n"
+    message: str = f'Warning! ZM backup cache is at'
+    date_and_time: str = dt.now().strftime('%Y-%m-%d %H:%M:%S,000:')
+    full_message: str = f"{date_and_time} {message} {disk_usage_end}%\n"
 
     with open('/etc/motd', 'r') as file:
         text: str = file.read()
 
-    if message[:-3] not in text:
+    if message not in text:
         with open('/etc/motd', 'a') as file:
             file.write(full_message)
     else:
@@ -296,12 +297,14 @@ if disk_usage_end >= 90:
             text_list: list[tuple[int, str]] = list(enumerate(file.readlines()))
 
         for index, line in text_list:
-            if message[:-3] in line:
+            if message in line:
                 text_list[index] = (index, full_message)
 
         with open('/etc/motd', 'w') as file:
             new_lines: list[str] = [line for _, line in text_list]
             file.writelines(new_lines)
+
+    logger.warning(f'{message} {disk_usage_end}')
 
 logger.warning('Done!\n\n')
 prune_log(log_file_name, length=5000)
